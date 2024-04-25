0a21affcdecf071ebfde61907b81a38740f86095
Walter kibet <kibetwalter726@gmail.com>
2024-04-25 19:57:00 +0300

    update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,9 +4,35 @@ import MyTextField from '../components/forms/MyTextField'
 import MyPassField from '../components/forms/MyPassField'
 import MyButton from '../components/forms/MyButton'
 import {Link} from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import AxiosInstance from './forms/AxiosInstance'
+import { useNavigate } from 'react-router-dom'
+
+
 const Login = ()=>{
+    const navigate = useNavigate()
+
+    const {handleSubmit, control} = useForm()
+
+    const submission = (data) =>{
+        AxiosInstance.post(`login/`,
+         {
+            email: data.email,
+            password:data.password ,
+         })
+        .then((response)=>{
+            console.log(response)
+            localStorage.setItem('Token',response.data.token)
+            navigate('/home')
+         })
+        .catch((error)=>{
+            console.error("Error during login", error)
+         })
+    }
+
     return(
         <div className={'myBackground'}>
+            <form onSubmit={handleSubmit(submission)}>
             <Box className={"whiteBox"}>
 
                 <Box className={"itemBox"}>
@@ -15,19 +41,26 @@ const Login = ()=>{
                 <Box className={"itemBox"}>
                     <MyTextField
                       label = {"Email"}
+                      name = {"email"}
+                      control = {control}
                     />
                 </Box>
                 <Box className={"itemBox"}>
-                    <MyPassField label ={"password"}/>
+                    <MyPassField 
+                       label ={"Password"}
+                       name = {"password"}
+                       control = {control}
+                       />
                 </Box>
                 <Box className={"itemBox"}>
-                    <MyButton label={"login"}/>
+                    <MyButton label={"login"} type={"submit"}/>
                 </Box>
                 <Box className={"itemBox"}>
                     <Link to="/register">I dont have an account? register</Link>
                 </Box>
 
             </Box>
+            </form>
         </div>
     )
 }
