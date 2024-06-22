5ba4d45ee10571062f73746da5f5fa445f8c43ea
Walter kibet <kibetwalter726@gmail.com>
2024-06-22 12:15:29 +0300

    update

--- frontend/src/components/forms/AxiosInstance.jsx
+++ frontend/src/components/forms/AxiosInstance.jsx
@@ -1,39 +1,39 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const baseURL = 'http://127.0.0.1:8000/'
+// const baseURL = 'http://127.0.0.1:8000/'
 
-const AxiosInstance = axios.create({
-    baseURL: baseURL,
-    timeout: 5000,
-    headers:{
-        "Content-Type": "application/json",
-        accept: "application/json"
-    }
-}) 
+// const AxiosInstance = axios.create({
+//     baseURL: baseURL,
+//     timeout: 5000,
+//     headers:{
+//         "Content-Type": "application/json",
+//         accept: "application/json"
+//     }
+// }) 
 
-AxiosInstance.interceptors.request.use(
-    (config) =>{
-        const token = localStorage.getItem('Token')
+// AxiosInstance.interceptors.request.use(
+//     (config) =>{
+//         const token = localStorage.getItem('Token')
 
-        if(token){
-            config.headers.Authorization = `Token ${token}`
-        }
-        else{
-            config.headers.Authorization = ``
-        }
-        return config;
-    }
-)
+//         if(token){
+//             config.headers.Authorization = `Token ${token}`
+//         }
+//         else{
+//             config.headers.Authorization = ``
+//         }
+//         return config;
+//     }
+// )
 
-AxiosInstance.interceptors.response.use(
-    (response) =>{
-        return response
-    },
-    (error) =>{
-        if(error.response && error.reponse.status === 401){
-            localStorage.removeItem('Token')
-        }
-    }
-)
+// AxiosInstance.interceptors.response.use(
+//     (response) =>{
+//         return response
+//     },
+//     (error) =>{
+//         if(error.response && error.reponse.status === 401){
+//             localStorage.removeItem('Token')
+//         }
+//     }
+// )
 
-export default AxiosInstance;
+// export default AxiosInstance;
